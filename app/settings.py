f5080997a9528f70bc8aa8d82ba382e4672e3d8c
João Vitor <138629963+vtrdll@users.noreply.github.com>
2025-04-18 18:57:22 -0300

    Update settings.py
    retirando usuario e senha postgresql

--- app/settings.py
+++ app/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'avarias',
-        'USER': 'postgres',
-        'PASSWORD':'polaca10',
-        'HOST':'localhost',
-        'PORT':'5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
